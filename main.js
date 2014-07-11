b2ac79006291f9c419108e806400ae48cc4870ad
lc00 <leannecheung00@gmail.com>
2014-07-11 16:40:06 -0600

    delete works; in the middle of undo deleted quote

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
 
 
-//add an edit page with delete button in it
+
 
 
 //update(sorted by rating) quote page when quotes are
-		// added
-		//deleted
 		//rated
 
 //click on the author of a quote to go
 //to a separate page/screen to shows
 //all quotes from that person
 
-//rate quote 1-5 stars
+//rate/thumb up and down quote 1-5 stars
 
 //click "Random Quote" button kicks off a pop
 //up(not an alert) of the quote at random draw.
@@ -23,26 +21,27 @@
 
 var quoteList = [];
 var index = 0;
+var theQuoteObj;
 
 var iq = (function(){
 
 	var createElement = function(newQuote, index){
 		var el = $('<li class="quoteBlock">');
-		el.data('data-index', index);
 
 		var rating = $('<div class="rating-display">');
 
 		var quoteNAuthor = $('<div class="quoteNAuthor">')
-		var quote = $('<p class="quote" data-type="text">' + newQuote.quote + '</p>');
-		var author = $('<p class="author">' + newQuote.author + '</p>');
+		var quote = $('<p class="quote editable" >' + newQuote.quote + '</p>');
+		var author = $('<p class="author editable">' + newQuote.author + '</p>');
 		quoteNAuthor.append(quote, author);
 
 		var bunchOfButtons = $('<div class="buttons">');
 		var rateButton = $('<button type="button" class="rating-button">Rate It</button>');
-		var editButton = $('<button type="button" class="edit-button">Edit</button>');
-		bunchOfButtons.append(rateButton, editButton);
+		var deleteButton = $('<button type="button" class="delete-button">Delete</button>');
+		bunchOfButtons.append(rateButton, deleteButton);
 
 		el.append(rating, quoteNAuthor, bunchOfButtons);
+		el.data('listIndex', index);
 
 		return el;
 
@@ -52,7 +51,7 @@ var iq = (function(){
 	}
 
 	//add new quote/author(both required) through form 
-	var newQuoteSubmit = function(eventArguments){
+	var addQuote = function(eventArguments){
 		eventArguments.preventDefault();
 
 		var quoteInput = $(this).find('[name=quote]').val();
@@ -73,8 +72,7 @@ var iq = (function(){
 
 			}
 
-			//increment index;
-			index += 1;
+			
 
 			//add new quote to quotelist array
 			quoteList.push(newQuoteItem);
@@ -82,7 +80,8 @@ var iq = (function(){
 			//append new quote to the quote list on dom
 			$('.quoteDisplay').append(createElement(newQuoteItem, index));
 
-		
+			//increment index;
+			index += 1;
 
 			return newQuoteItem;
 		}	
@@ -94,7 +93,10 @@ var iq = (function(){
 	var editQuote = function(){
 		var originalField = $(this);
 		var input = $('<textarea class="edit-quote"/>');
+		$(this).after(input);
+
 		input.height($(this).height());
+		input.width($(this).width());
 		$(this).hide();
 		input.val($(this).text());
 		input.focus();
@@ -106,12 +108,33 @@ var iq = (function(){
 		})
 
 	}
+
+	var deleteQuote = function(){
+		var id = $(this).closest('li').data('listIndex');
+
+		$(this).closest('li').detach();
+
+		theQuoteObj = _.findWhere(quoteList, {index: id});
+		var theIndex = _.indexOf(quoteList, theQuoteObj);
+		quoteList.splice(theIndex, 1);
+	}
+
+	var undoLastDeletedQuote = function(){
+		//create element with theQuoteObj
+		//add it to the quoteList
+
+
+		 
+	}
 	
 	return {
-		quote:	newQuoteSubmit,
-		edit: editQuote
+		addQuote: addQuote,
+		editQuote: editQuote,
+		deleteQuote: deleteQuote,
+		undoLastDeletedQuote: undoLastDeletedQuote
 
 	};
+
 	
 
 })();
@@ -128,9 +151,11 @@ var iq = (function(){
 
 $(document).on('ready', function() {
 
-  $('#addQuote').on('submit', iq.quote);
+  $('#addQuote').on('submit', iq.addQuote);
 
-  $(document).on('click', ".editable", iq.edit);
+  $(document).on('click', ".editable", iq.editQuote);
+
+  $(document).on('click', ".delete-button", iq.deleteQuote);
 
   
 
